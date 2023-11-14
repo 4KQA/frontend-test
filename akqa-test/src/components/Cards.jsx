3f83b052d1eab9d20e794bb3bf81c78ee7d6f534
pernwn <pernillepjohansen@hotmail.com>
2023-11-14 15:53:57 +0100

    forslag

--- akqa-test/src/components/Cards.jsx
+++ akqa-test/src/components/Cards.jsx
@@ -5,40 +5,44 @@ import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 export function BubbleCard(props) {
     return (
 
-            <div className="bub-card-content">
-                <div className="bub-img"><img src={props.image} alt={props.title} /></div>
-                <div className="bub-txt-container">
-                    <h3>{props.title}</h3>
-                    <p>{props.descrip}</p>
-                </div>
-                <a href="#" onClick={() => alert(`You have clicked on ${props.link}`)} className='pinkLink'>{props.link}</a>
+        <div className="bub-card-content">
+            <div className="bub-img"><img src={props.image} alt={props.title} /></div>
+            <div className="bub-txt-container">
+                <h3>{props.title}</h3>
+                <p>{props.descrip}</p>
             </div>
+            <a href="#" onClick={() => alert(`You have clicked on ${props.link}`)} className='pinkLink'>{props.link}</a>
+        </div>
 
 
     )
 }
 
-export function DefaultCard(props){
-    return(
+export function DefaultCard(props) {
+    return (
 
-            <div className="card-container">
-                <div className='card-img'><img src={props.image} alt={props.title} /></div>
+        <div className="card-container">
+            <div className='card-img'><img src={props.image} alt={props.title} /></div>
+            <div className='c-txt'>
                 <h3>{props.title}</h3>
                 <p>{props.descrip}</p>
                 <a href="#" onClick={() => alert(`You have clicked on ${props.link}`)} className='pinkLink'>{props.link}</a>
             </div>
+        </div>
 
     )
 }
 
-export function VideoCard(props){
-    return(
+export function VideoCard(props) {
+    return (
         <div className='vCard'>
             <div className='e-video'> {/*A video should go here instead of div*/}
-                <PlayCircleIcon/>
-            </div> 
-            <h6>{props.title}</h6>
-            <p>{props.descrip}</p>
+                <PlayCircleIcon sx={{ color: "var(--secondaryC)", fontSize:"30pt" }}/>
+            </div>
+            <div className='e-txt'>
+                <h5>{props.title}</h5>
+                <p>{props.descrip}</p>
+            </div>
         </div>
     )
 }
