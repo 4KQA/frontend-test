0b0f9c333fdff44a1c259dd9c04e6fb13650f38a
”Frederik <fred295q@stud.kea.dk>
2023-11-29 12:42:11 +0100

    video background

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ function Video() {
   return (
     <div className="flex flex-col lg:flex-row gap-5">
       {/* <img className="p-5 bg-leoPink rounded-full" src={play} alt="" /> */}
-      <video src="#" poster={play}></video>
+      <video className="bg-white" src="#" poster={play}></video>
       <div>
         <h3 className="text-leoBlue">Video title</h3>
         <p>Short description of the contents or subject of the video. You can upload a screen from the video as thumbnail image</p>
