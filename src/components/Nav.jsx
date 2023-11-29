ab71ad7e98cb4f21e4357db5d29be7ea5511259e
”Frederik <fred295q@stud.kea.dk>
2023-11-29 12:24:14 +0100

    Burger added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,9 +31,21 @@ function Nav() {
     <nav className="mt-2">
       <div className="flex justify-between mx-10 mb-5">
         <img src={logo} alt="DermaWorld by Leo Pharma logo" />
-        <div className="flex flex-row border-solid border-2 border-gray rounded-2xl px-2">
-          <input className=" hidden md:block p-1 m-1" type="search" placeholder="Search" />
-          <img src={search} alt="Search logo" />
+        <div className="flex flex-row gap-10">
+          <div className="flex flex-row md:border-solid md:border-2 md:border-gray md:rounded-2xl px-2">
+            <input className=" hidden md:block p-1 m-1" type="search" placeholder="Search" />
+            <img className=" w-14 md:w-full" src={search} alt="Search logo" />
+          </div>
+          <button onClick={() => document.querySelector("ul").classList.toggle("hidden")} className=" w-full md:hidden">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round"></path> <path d="M4 12L20 12" stroke="#000000" strokeWidth="2" strokeLinecap="round"></path> <path d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round"></path>{" "}
+              </g>
+            </svg>
+          </button>
         </div>
       </div>
       <ul className=" hidden md:flex flex-row gap-5 font-normal px-10">
